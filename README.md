9d0e1b8c18326e3f2b08db4f39ae1698dcf8a003
Bernardo <bernardo.ribeiro@outlook.com.br>
2025-05-20 14:00:29 -0300

    Initial implementation
    Made the necessary changes for BGUI to work with Range Engine 1.6

--- README.md
+++ README.md
@@ -1,5 +1,47 @@
-bgui is a Python library to handle GUIs in the Blender Game Engine.
+# bgui
 
-Author: Mitchell Stokes (Moguri); Email: mogurijin@gmail.com
+**bgui** is a Python library for handling GUIs in the Blender Game Engine.
 
-Docs: http://bgui.readthedocs.org/en/latest/
+**Author:** Mitchell Stokes (Moguri)
+**Email:** [mogurijin@gmail.com](mailto:mogurijin@gmail.com)
+
+**Documentation:** [http://bgui.readthedocs.org/en/latest/](http://bgui.readthedocs.org/en/latest/)
+
+---
+
+## Important Notice
+
+**This project is still under development!**
+Some functions may not work yet because they haven’t been fixed.
+For example: recognizing mouse clicks on the button.
+
+---
+
+
+## Changelog
+
+### New Features
+
+* Added the class that adapts BGUI to work with Range (`RangeBGUI`).
+* Modified the text directory to better support the `TextLibrary` function call.
+  *Note:* This code was previously in the `__init__` method but did not work properly there. Since Range does not have `logic.textLib`, the `BlfTextLibrary` and `TextLibrary` classes are used instead.
+* Removed the use of PyQt4.
+
+---
+
+**Implementation with Range Engine**
+
+**Author:** Bernardo Ribeiro
+**Email:** bernardo.ribeiro@outlook.com.br
+
+---
+
+## How to Use
+
+Open the directory `\examples\range 1.6`, where you’ll find the standard folder hierarchy used in the project.
+Simply keep the `bgui` folder and the `range_bgui_adapter.py` file in the root directory of your project, and it will work right away.
+
+(A basic `.Range` file was created just to demonstrate how it works.)
+
+
+## Feel free to contribute to the project!
---
+++ examples/range 1.6/range_bgui_adapter.py
@@ -0,0 +1,83 @@
+import os
+import sys
+
+# Add BGUI to the path - adjust based on your installation
+bgui_path = os.path.join(os.path.dirname(__file__), 'bgui')
+if bgui_path not in sys.path:
+    sys.path.append(bgui_path)
+
+import Range as rg
+import bgl
+import bgui
+from bgui.text.blf import BlfTextLibrary
+
+class RangeBGUI:
+    def __init__(self, range_scene):
+        self.scene = range_scene
+
+        self.textlib = BlfTextLibrary()
+
+        self.system = bgui.System(self.textlib)
+
+        self.frame = bgui.Frame(self.system, "main_frame", border=1)
+        self.frame.colors = [(0, 0, 0, 0) for _ in range(4)]
+
+        self.elements = {}
+        self.scene.post_draw.append(self._render_ui)
+
+    def _setup_callbacks(self):
+        self.scene.add_post_draw_callback(self._render_ui)
+        self.scene.add_input_callback(self._handle_input)
+
+    def _render_ui(self):
+        bgl.glPushAttrib(bgl.GL_ALL_ATTRIB_BITS)
+        bgl.glEnable(bgl.GL_BLEND)
+        bgl.glBlendFunc(bgl.GL_SRC_ALPHA, bgl.GL_ONE_MINUS_SRC_ALPHA)
+        bgl.glEnable(bgl.GL_LINE_SMOOTH)
+        bgl.glHint(bgl.GL_LINE_SMOOTH_HINT, bgl.GL_NICEST)
+        bgl.glEnable(bgl.GL_POLYGON_SMOOTH)
+
+        bgl.glTexParameteri(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MIN_FILTER, bgl.GL_NEAREST)
+        bgl.glTexParameteri(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MAG_FILTER, bgl.GL_NEAREST)
+
+        self.system.render()
+
+        bgl.glPopAttrib()
+
+
+
+    def _handle_input(self, event):
+        if event.type == rg.EVENT_MOUSE_MOVE:
+            self.system.update_mouse(event.mouse_x, event.mouse_y)
+        elif event.type == rg.EVENT_MOUSE_PRESS:
+            self.system.update_mouse(event.mouse_x, event.mouse_y)
+            self.system.activate_mouse_button(event.button)
+        elif event.type == rg.EVENT_MOUSE_RELEASE:
+            self.system.update_mouse(event.mouse_x, event.mouse_y)
+            self.system.deactivate_mouse_button(event.button)
+        elif event.type == rg.EVENT_KEY_PRESS:
+            self.system.activate_key(event.key)
+        elif event.type == rg.EVENT_KEY_RELEASE:
+            self.system.deactivate_key(event.key)
+
+    def add_element(self, element_class, name, **kwargs):
+        parent = kwargs.pop('parent', self.frame)
+        widget = element_class(parent, name, **kwargs)
+        self.elements[name] = widget
+        return widget
+
+
+    def get_element(self, name):
+        return self.elements.get(name)
+
+    def remove_element(self, name):
+        if name in self.elements:
+            element = self.elements[name]
+            element.parent.remove_widget(element)
+            del self.elements[name]
+
+    def cleanup(self):
+        self.scene.remove_post_draw_callback(self._render_ui)
+        self.scene.remove_input_callback(self._handle_input)
+        self.system = None
+        self.elements = {}
